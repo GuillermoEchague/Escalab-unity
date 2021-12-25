cbefc7d25321ebf54c8a02c1ea33342fa1bf47bc
Guillermo Echague <46538216+GuillermoEchague@users.noreply.github.com>
2021-12-24 21:27:27 -0300

    First Scenario

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -8,16 +8,27 @@ public class playerController : MonoBehaviour
     public float moveSpeed;
 
     [Header("Salto")]
+    private bool canDoubleJump; 
     public float jumpForce; 
     
     [Header("Componentes")]
     public Rigidbody2D theRB;
 
+    [Header("Animator")]
+    private Animator anim;
+    private SpriteRenderer theSR;
+
+    [Header("Grounded")]
+    private bool isGrounded;
+    public Transform groundCheckpoint;
+    public LayerMask whatIsGround;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        theSR = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -25,9 +36,40 @@ public class playerController : MonoBehaviour
     {
         theRB.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), theRB.velocity.y);
 
-        if(Input.GetButtonDown("Jump"))
+        isGrounded = Physics2D.OverlapCircle(groundCheckpoint.position, .2f, whatIsGround);
+
+        if(isGrounded)
         {
-            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+            canDoubleJump = true;
         }
+
+         if (Input.GetButtonDown("Jump")) {
+                      
+
+            if(isGrounded)
+            {
+                theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+                  // Debug.Log("Jump!2");
+            } 
+            else 
+            {
+                    if(canDoubleJump)
+                    {
+                       theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+                       canDoubleJump = false;
+                        // Debug.Log("Jump!3");
+                    }
+             }
+        }
+
+        if(theRB.velocity.x < 0)
+        {
+            theSR.flipX = true;
+        } else if(theRB.velocity.x > 0) {
+            theSR.flipX = false;
+        } 
+
+        anim.SetFloat("moveSpeed", Mathf.Abs(theRB.velocity.x));
+        anim.SetBool("isGrounded",isGrounded);
     }
 }
