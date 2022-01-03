6c3e1a0854a96a07ea9c95f478be7e8150f79e6d
Guillermo Echague <46538216+GuillermoEchague@users.noreply.github.com>
2022-01-03 01:38:22 -0300

    All Scenes based ok

---
+++ Assets/Scripts/BossTankHitBoss.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTankHitBoss : MonoBehaviour
+{
+    public BossTankController bossCont;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player" && playerController.instance.transform.position.y > transform.position.y)
+        {
+            bossCont.TakeHit();
+            playerController.instance.Bounce();
+            gameObject.SetActive(false);
+        }
+    }
+}
